e8e645d7a4068840b8b9faf6ea32ed915748ddb5
karinaoshiro <karinaoshiro43@gmail.com>
2024-07-14 19:16:04 -0400

    update1

--- src/comp/product.js
+++ src/comp/product.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Productdetail from './productdetails'
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegEye } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import './product.css'
 
-const Product = () => {
-    const [product, setProduct] = useState(Productdetail)
+const Product = ({product, setProduct}) => {
     const filtterproduct = (product) => 
         {
             const update = Productdetail.filter((x) => 
